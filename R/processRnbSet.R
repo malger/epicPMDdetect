e62f73a142932207bb0e84fc5f5003fdbd69fc5b
Malte Groß <malte.gross@web.de>
2020-03-30 11:15:04 +0200

    implemented segmentRnbSet

--- R/processRnbSet.R
+++ R/processRnbSet.R
@@ -1,16 +1,36 @@
+default.options = list(
+  training.chr.sel="chr2",
+  pdfFilename = NULL,
+  seqLengths = "hg19",
+  knn=c(30,60,90),
+  q=1,
+  cutoff = c(50000,70000,90000),
+  num.cores = 1,
+  markLowDensitySegm = T
+)
+
 #' Segments all samples of an Rnbeads Object
 #' @param rnbobj the rnbeads object with the methylation data
 #' @param outputFolder the folder to which the segmentation files should be written to
 #' @param num.cores number of cores to use in parallel
 #' @param settings pass settings to segmentation Function. Not yet implemented
 #' @export
-#' @import RnBeads
+#' @import RnBeads,utils
 segmentRnbSet = function(rnbobj,outputFolder,num.cores=1,settings = NULL){
-  for (i in 1:length(samples(data))) {
-    s = samples(data)[i]
+  for (i in 1:length(samples(rnbobj))) {
+    s = samples(rnbobj)[i]
     message('processing sample: ',s)
-    m = buildMethGrangesFromRnbSet(data,s)
-    segm = segmentPMDsKNN(m,num.cores = num.cores)
+    m = buildMethGrangesFromRnbSet(rnbobj,s)
+    if(is.null(settings)){
+      segm = segmentPMDsKNN(m,num.cores = num.cores)
+    } else {
+      if(typeof(settings) != "list")
+        stop("settings musst be a list with following named children: knn,cutoff,seqLengths")
+      #TODO pass more settings
+      args = c(list(m=m),modifyList(default.options,settings))
+      args[["num.cores"]]=num.cores
+      segm = do.call(segmentPMDsKNN,args)
+    }
     writeSegmentation(segm,file.path(outputFolder,paste0(s,'.segm')))
     writePMDs2bed(segm,file.path(outputFolder,paste0(s,'.pmds')))
     writeNpmds2bed(segm,file.path(outputFolder,paste0(s,'.oths')))
